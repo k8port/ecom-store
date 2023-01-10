5b9cd98da08b265cb21df1d783e74d580654ccc9
kateportalatin <kate.portalatin@blueyonder.com>
2023-01-10 10:19:22 -0600

    first commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,15 @@
 // Create the Footer component here
+import React from 'react';
+import { AppBar, Toolbar, Typography } from "@mui/materials";
+
+function Footer() {
+    return (
+        <AppBar position="sticky" elevation="{0}" component="footer" color="default">
+            <Toolbar style={{ justifyContent: "center" }}>
+                <Typography variant="caption">Copyright © 2022 - The Store</Typography>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Footer;
